8b4781b4d9d335995355a7e56ed4ed4ff5bf4fcd
Dogan GOGCE <dogangokce76@gmail.com>
2015-09-08 16:29:34 +0500

    Update migration_lang.php 1.7.4
    Update migration_lang.php 1.7.4 Translate Dogan GOGCE

--- turkish/migration_lang.php
+++ turkish/migration_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['migration_none_found']			= "No migrations were found.";
-$lang['migration_not_found']			= "This migration could not be found.";
-$lang['migration_multiple_version']		= "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']	= "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']	= "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method']	= "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']		= "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']			= "Hiçbir göç bulunamadı.";
+$lang['migration_not_found']			= "Bu göç bulunamadı";
+$lang['migration_multiple_version']		= "Bu aynı sürüm numarasına sahip birden fazla göçler şunlardır: %d.";
+$lang['migration_class_doesnt_exist']	= "Göç sınıfı \"%s\" bulunamadı.";
+$lang['migration_missing_up_method']	= "Göç sınıfı \"%s\" yükseltme yöntemi eksik.";
+$lang['migration_missing_down_method']	= "Göç sınıfı \"%s\" düşürme yöntemi eksik.";
+$lang['migration_invalid_filename']		= "\"%s\" göçü geçersiz dosya adına sahip.";
 
 
 /* End of file migration_lang.php */
-/* Location: ./system/language/english/migration_lang.php */
+/* Location: ./system/language/turkish/migration_lang.php */
