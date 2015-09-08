383ad57a9b1872333e9af8932b58145fbc4325a1
Dogan GOGCE <dogangokce76@gmail.com>
2015-09-08 16:06:39 +0500

    Update unit_test_lang.php 1.7.4
    Update unit_test_lang.php 1.7.4 Translate Dogan GOGCE

--- turkish/unit_test_lang.php
+++ turkish/unit_test_lang.php
@@ -1,25 +1,25 @@
 <?php
 
-$lang['ut_test_name']		= 'Test Name';
-$lang['ut_test_datatype']	= 'Test Datatype';
-$lang['ut_res_datatype']	= 'Expected Datatype';
-$lang['ut_result']			= 'Result';
+$lang['ut_test_name']		= 'Test Adı';
+$lang['ut_test_datatype']	= 'Test Veritürü';
+$lang['ut_res_datatype']	= 'Beklenen Veritürü';
+$lang['ut_result']			= 'Sonuç';
 $lang['ut_undefined']		= 'Undefined Test Name';
-$lang['ut_file']			= 'File Name';
-$lang['ut_line']			= 'Line Number';
-$lang['ut_passed']			= 'Passed';
-$lang['ut_failed']			= 'Failed';
+$lang['ut_file']			= 'Dosya Adı';
+$lang['ut_line']			= 'Satır Numarası';
+$lang['ut_passed']			= 'Geçti';
+$lang['ut_failed']			= 'Başarısız';
 $lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
+$lang['ut_integer']			= 'Tamsayı';
 $lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // can be the same as float
-$lang['ut_string']			= 'String';
-$lang['ut_array']			= 'Array';
-$lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Resource';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'Notes';
+$lang['ut_double']			= 'Float'; // şamandıra ile aynı olabilir
+$lang['ut_string']			= 'Dize';
+$lang['ut_array']			= 'Dizi';
+$lang['ut_object']			= 'Obje';
+$lang['ut_resource']		= 'Kaynak';
+$lang['ut_null']			= 'Boş';
+$lang['ut_notes']			= 'Notlar';
 
 
 /* End of file unit_test_lang.php */
-/* Location: ./system/language/english/unit_test_lang.php */
+/* Location: ./system/language/turkish/unit_test_lang.php */
