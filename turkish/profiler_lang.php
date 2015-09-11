f4c34607bcedc54cbe56a4dd1af30de68fa416dc
Dogan GOGCE <dogangokce76@gmail.com>
2015-09-11 13:46:34 +0500

    Update profiler_lang.php

--- turkish/profiler_lang.php
+++ turkish/profiler_lang.php
@@ -1,25 +1,26 @@
 <?php
 
-$lang['profiler_database']		= 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
-$lang['profiler_benchmarks']	= 'BENCHMARKS';
-$lang['profiler_queries']		= 'QUERIES';
-$lang['profiler_get_data']		= 'GET DATA';
-$lang['profiler_post_data']		= 'POST DATA';
-$lang['profiler_uri_string']	= 'URI STRING';
-$lang['profiler_memory_usage']	= 'MEMORY USAGE';
-$lang['profiler_config']		= 'CONFIG VARIABLES';
-$lang['profiler_session_data']	= 'SESSION DATA';
-$lang['profiler_headers']		= 'HTTP HEADERS';
-$lang['profiler_no_db']			= 'Veritabanı sürücüsü yüklü değil';
-$lang['profiler_no_queries']	= 'Hiçbir sorgu çalıştırlmadı';
-$lang['profiler_no_post']		= 'Hiçbir POST verisi yok';
-$lang['profiler_no_get']		= 'Hiçbir GET veri yok';
-$lang['profiler_no_uri']		= 'Hiçbir URI veri yok';
-$lang['profiler_no_memory']		= 'Bellek Kullanımı Yok';
-$lang['profiler_no_profiles']	= 'Profil bilgisi yok - Tüm profil bölümleri devredışı bırakıldı.';
+$lang['profiler_database'] = 'VERİTABANI';
+$lang['profiler_controller_info'] = 'SINIF/METOD';
+$lang['profiler_benchmarks'] = 'KARŞILAŞTIRMALI DEĞERLENDİRMELER';
+$lang['profiler_queries'] = 'SORGULAR';
+$lang['profiler_get_data'] = 'GET VERİLERİ';
+$lang['profiler_post_data'] = 'POST VERİLERİ';
+$lang['profiler_uri_string'] = 'URI VERİLERİ';
+$lang['profiler_memory_usage'] = 'Hafıza kullanımı';
+$lang['profiler_config'] = 'CONFIG DEĞİŞKENLERİ';
+$lang['profiler_session_data']	= 'SESSION VERİLERİ';
+$lang['profiler_headers'] = 'HTTP BAŞLIKLARI';
+$lang['profiler_no_db'] = 'Veritabanı sürücüsü şu anda yüklenmedi';
+$lang['profiler_no_queries'] = 'Hiçbir sorgu çalışmıyor';
+$lang['profiler_no_post'] = 'Hiçbir POST verisi yok';
+$lang['profiler_no_get'] = 'Hiçbir GET verisi yok';
+$lang['profiler_no_uri'] = 'Hiçbir URI verisi yok';
+$lang['profiler_no_memory'] = 'Hafıza kullanım dışı';
+$lang['profiler_no_profiles']	= 'Profil verisi yok - Tüm Profiler bölümleri devre dışı bırakılmış.';
 $lang['profiler_section_hide']	= 'Gizle';
 $lang['profiler_section_show']	= 'Göster';
+$lang['profiler_seconds'] = 'Saniye';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/turkish/profiler_lang.php */
