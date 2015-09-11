4c7d730161f9459f64d31c8f94a1a1a3dd02e264
Dogan GOGCE <dogangokce76@gmail.com>
2015-09-11 13:46:06 +0500

    Update migration_lang.php

--- turkish/migration_lang.php
+++ turkish/migration_lang.php
@@ -1,12 +1,13 @@
 <?php
 
-$lang['migration_none_found']			= "Hiçbir göç bulunamadı.";
-$lang['migration_not_found']			= "Bu göç bulunamadı";
-$lang['migration_multiple_version']		= "Bu aynı sürüm numarasına sahip birden fazla göçler şunlardır: %d.";
-$lang['migration_class_doesnt_exist']	= "Göç sınıfı \"%s\" bulunamadı.";
-$lang['migration_missing_up_method']	= "Göç sınıfı \"%s\" yükseltme yöntemi eksik.";
-$lang['migration_missing_down_method']	= "Göç sınıfı \"%s\" düşürme yöntemi eksik.";
-$lang['migration_invalid_filename']		= "\"%s\" göçü geçersiz dosya adına sahip.";
+$lang['migration_none_found'] = "Hiç migrasyon bulunamadı.";
+$lang['migration_not_found'] = '%s Bu migrasyon bulunamadı.';
+$lang['migration_sequence_gap'] = 'There is a gap in the migration sequence near version number: %s.';
+$lang['migration_multiple_version'] = 'Aynı versiyon numarasına sahip migrasyonlar:  %s.';
+$lang['migration_class_doesnt_exist'] = 'Migrasyon sınıfı "%s" bulunamadı.';
+$lang['migration_missing_up_method'] = 'Migrasyon sınıfında "%s" -> "up" metodu bulunmuyor.';
+$lang['migration_missing_down_method'] = 'Migrasyon sınıfında "%s" -> "down" metodu bulunmuyor.';
+$lang['migration_invalid_filename'] = '"%s" migrasyonu hatalı bir dosya adına sahip.';
 
 
 /* End of file migration_lang.php */
